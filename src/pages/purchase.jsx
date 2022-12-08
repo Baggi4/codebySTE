6dc6b363a080103fdb3456c94c0ad449451beae6
StevenMad <stevenmadec@live.fr>
2022-12-08 09:39:29 +0100

    1.4

--- src/pages/purchase.jsx
+++ src/pages/purchase.jsx
@@ -17,8 +17,9 @@ export default function Purchase() {
       </Head>
       <Container className="mt-16 sm:mt-32">
             <h1 className="text-4xl font-bold text-zinc-800 dark:text-zinc-100">
-              Purchase theses themes, develop and design by yourself the Future.
+              Purchase theses themes, develop and design by yourself the Future.get started with one of our Templates.
             </h1>
+            <p>get started with one of my Templates.</p>
       </Container>
     </>
   )
